556a040c99f1ab741f3cc79485d22dc454fca0bb
Mark Bernard <markbernard@users.noreply.github.com>
2017-05-20 19:11:16 -0400

    Add chapter 7 changes.

---
+++ chapter07/src/io/github/markbernard/jnotepad/action/EditAction.java
@@ -0,0 +1,433 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Mark Bernard
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in the 
+ * Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.markbernard.jnotepad.action;
+
+import io.github.markbernard.jnotepad.IconGenerator;
+import io.github.markbernard.jnotepad.JNotepad;
+import io.github.markbernard.jnotepad.dialog.SearchDialog;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.KeyStroke;
+
+/**
+ * Takes care of all actions for the Edit menu. Menu items will be internal classes of this class.
+ * 
+ * @author Mark Bernard
+ */
+public class EditAction extends AbstractAction {
+    private static final long serialVersionUID = 2228283050016546717L;
+
+    /**
+     * Set up values appropriate to the Undo menu item.
+     */
+    public EditAction() {
+        putValue(Action.MNEMONIC_KEY, KeyEvent.VK_E);
+        putValue(Action.NAME, "Edit");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    /**
+     * Actions for the Edit > Undo menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class UndoAction extends AbstractAction {
+        private static final long serialVersionUID = 5936711805304319306L;
+        
+        private JNotepad jNotepad;
+        
+        /**
+         * Set up values appropriate to the Undo menu item.
+         * 
+         * @param jNotepad 
+         */
+        public UndoAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_U);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Undo");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.undo();
+                }
+            }, "Undo").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Cut menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class CutAction extends AbstractAction {
+        private static final long serialVersionUID = 6520482354999730690L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Cut menu item.
+         * 
+         * @param jNotepad 
+         */
+        public CutAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_T);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Cut");
+            putValue(Action.SHORT_DESCRIPTION, "Remove the current selection from the document and put it in the clipboard.");
+            putValue(Action.SMALL_ICON, new IconGenerator("/res/icons/CutSmall.png").loadImage());
+            putValue(Action.LARGE_ICON_KEY, new IconGenerator("/res/icons/Cut.png").loadImage());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.cut();
+                }
+            }, "Cut").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Copy menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class CopyAction extends AbstractAction {
+        private static final long serialVersionUID = -2906245309033009826L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Copy menu item.
+         * 
+         * @param jNotepad 
+         */
+        public CopyAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_C);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Copy");
+            putValue(Action.SHORT_DESCRIPTION, "Copy the current selection from the document and put it in the clipboard.");
+            putValue(Action.SMALL_ICON, new IconGenerator("/res/icons/CopySmall.png").loadImage());
+            putValue(Action.LARGE_ICON_KEY, new IconGenerator("/res/icons/Copy.png").loadImage());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.copy();
+                }
+            }, "Copy").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Paste menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class PasteAction extends AbstractAction {
+        private static final long serialVersionUID = -555624621599811242L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Paste menu item.
+         * 
+         * @param jNotepad 
+         */
+        public PasteAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_P);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Paste");
+            putValue(Action.SHORT_DESCRIPTION, "Paste the clipboard contents into the current cursor location or selection.");
+            putValue(Action.SMALL_ICON, new IconGenerator("/res/icons/PasteSmall.png").loadImage());
+            putValue(Action.LARGE_ICON_KEY, new IconGenerator("/res/icons/Paste.png").loadImage());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.paste();
+                }
+            }, "Paste").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Delete menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class DeleteAction extends AbstractAction {
+        private static final long serialVersionUID = -1020774942566648167L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Delete menu item.
+         * 
+         * @param jNotepad 
+         */
+        public DeleteAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_L);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0));
+            putValue(Action.NAME, "Delete");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.delete();
+                }
+            }, "Paste").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Find menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class FindAction extends AbstractAction {
+        private static final long serialVersionUID = 1215020210624036643L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Find menu item.
+         * 
+         * @param jNotepad 
+         */
+        public FindAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_F);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Find...");
+            putValue(Action.SHORT_DESCRIPTION, "Open the find dialog.");
+            putValue(Action.SMALL_ICON, new IconGenerator("/res/icons/FindSmall.png").loadImage());
+            putValue(Action.LARGE_ICON_KEY, new IconGenerator("/res/icons/Find.png").loadImage());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.find();
+                }
+            }, "Find").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Find Next menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class FindNextAction extends AbstractAction {
+        private static final long serialVersionUID = 1215020210624036643L;
+        private JNotepad jNotepad;
+        private SearchDialog searchDialog;
+
+        /**
+         * Set up values appropriate to the Find Next menu item.
+         * 
+         * @param jNotepad 
+         * @param searchDialog 
+         */
+        public FindNextAction(JNotepad jNotepad, SearchDialog searchDialog) {
+            this.jNotepad = jNotepad;
+            this.searchDialog = searchDialog;
+            if (searchDialog == null) {
+                putValue(Action.MNEMONIC_KEY, KeyEvent.VK_N);
+            } else {
+                putValue(MNEMONIC_KEY, KeyEvent.VK_F);
+            }
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F3, 0));
+            putValue(Action.NAME, "Find Next");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    if (searchDialog != null) {
+                        jNotepad.setFindTerm(searchDialog.getFindTerm());
+                        jNotepad.setFindDownDirection(searchDialog.isFindDirectionDown());
+                        jNotepad.setMatchCase(searchDialog.isMatchCase());
+                    }
+                    jNotepad.findNext();
+                }
+            }, "Find Next (Menu)").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Replace menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class ReplaceAction extends AbstractAction {
+        private static final long serialVersionUID = 1215020210624036643L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Replace menu item.
+         * 
+         * @param jNotepad 
+         */
+        public ReplaceAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_R);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Replace...");
+            putValue(Action.SHORT_DESCRIPTION, "Open the replace dialog.");
+            putValue(Action.SMALL_ICON, new IconGenerator("/res/icons/TextReplaceSmall.png").loadImage());
+            putValue(Action.LARGE_ICON_KEY, new IconGenerator("/res/icons/TextReplace.png").loadImage());
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.replace();
+                }
+            }, "Replace (menu)").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Go To menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class GoToAction extends AbstractAction {
+        private static final long serialVersionUID = 1215020210624036643L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Go To menu item.
+         * 
+         * @param jNotepad 
+         */
+        public GoToAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_G);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_G, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Go To...");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.goTo();
+                }
+            }, "Go To (menu)").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Select All menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class SelectAllAction extends AbstractAction {
+        private static final long serialVersionUID = 1215020210624036643L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Select All menu item.
+         * 
+         * @param jNotepad 
+         */
+        public SelectAllAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_A);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_A, InputEvent.CTRL_DOWN_MASK));
+            putValue(Action.NAME, "Select All");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.selectAll();
+                }
+            }, "Select All").start();
+        }
+    }
+
+    /**
+     * Actions for the Edit > Time Date menu item.
+     * 
+     * @author Mark Bernard
+     */
+    public static class TimeDateAction extends AbstractAction {
+        private static final long serialVersionUID = 1215020210624036643L;
+        private JNotepad jNotepad;
+
+        /**
+         * Set up values appropriate to the Time Date menu item.
+         * 
+         * @param jNotepad 
+         */
+        public TimeDateAction(JNotepad jNotepad) {
+            this.jNotepad = jNotepad;
+            putValue(Action.MNEMONIC_KEY, KeyEvent.VK_D);
+            putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F5, 0));
+            putValue(Action.NAME, "Time/Date");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    jNotepad.timeDate();
+                }
+            }, "Time/Date").start();
+        }
+    }
+}
---
+++ chapter07/src/io/github/markbernard/jnotepad/dialog/SearchDialog.java
@@ -0,0 +1,241 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Mark Bernard
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in the 
+ * Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or 
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package io.github.markbernard.jnotepad.dialog;
+
+import io.github.markbernard.jnotepad.JNotepad;
+import io.github.markbernard.jnotepad.action.DialogAction;
+import io.github.markbernard.jnotepad.action.EditAction;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JRootPane;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * @author Mark Bernard
+ */
+public class SearchDialog extends BasicDialog implements DocumentListener {
+    private static final long serialVersionUID = 7765170324238442423L;
+
+    private JTextField findField;
+    private JTextField replaceField;
+    private JCheckBox matchCase;
+    private JRadioButton upRadio;
+    private JRadioButton downRadio;
+    private JButton findNextButton;
+    private JButton replaceButton;
+    private JButton replaceAllButton;
+    private boolean replace;
+    
+    /**
+     * Set up GUI
+     * 
+     * @param frame 
+     * @param jNotepad 
+     * @param replace set to true to show replace dialog, false to only show find
+     */
+    public SearchDialog(JFrame frame, JNotepad jNotepad, boolean replace) {
+        super(frame, (replace ? "Replace" : "Find"), false);
+        this.replace = replace;
+        setLayout(new BorderLayout());
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        add(mainPanel, BorderLayout.CENTER);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(11, 5, 11, 5));
+
+        JPanel inputPanel = new JPanel(new BorderLayout());
+        mainPanel.add(inputPanel, BorderLayout.CENTER);
+        JPanel textInputPanel = new JPanel(new BorderLayout(10, 0));
+        inputPanel.add(textInputPanel, BorderLayout.NORTH);
+        JPanel findLabelPanel = new JPanel(new GridLayout(0, 1, 5, 5));
+        textInputPanel.add(findLabelPanel, BorderLayout.WEST);
+        JPanel findInputPanel = new JPanel(new GridLayout(0, 1, 5, 5));
+        textInputPanel.add(findInputPanel, BorderLayout.CENTER);
+        JLabel findLabel = new JLabel("Find what:");
+        findLabelPanel.add(findLabel, BorderLayout.WEST);
+        findField = new JTextField(21);
+        addEscapeToActionMap(findField);
+        findInputPanel.add(findField, BorderLayout.CENTER);
+        findLabel.setLabelFor(findField);
+        findLabel.setDisplayedMnemonic(KeyEvent.VK_N);
+        if (replace) {
+            JLabel replaceLabel = new JLabel("Replace with:");
+            findLabelPanel.add(replaceLabel, BorderLayout.WEST);
+            replaceField = new JTextField(21);
+            addEscapeToActionMap(replaceField);
+            findInputPanel.add(replaceField, BorderLayout.CENTER);
+            replaceLabel.setLabelFor(replaceField);
+            replaceLabel.setDisplayedMnemonic(KeyEvent.VK_P);
+        }
+
+        if (!replace) {
+            JPanel radioButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            inputPanel.add(radioButtonPanel, BorderLayout.EAST);
+            upRadio = new JRadioButton("Up");
+            addEscapeToActionMap(upRadio);
+            radioButtonPanel.add(upRadio);
+            upRadio.setMnemonic(KeyEvent.VK_U);
+            downRadio = new JRadioButton("Down");
+            addEscapeToActionMap(downRadio);
+            radioButtonPanel.add(downRadio);
+            downRadio.setMnemonic(KeyEvent.VK_D);
+            downRadio.setSelected(true);
+            radioButtonPanel.setBorder(BorderFactory.createTitledBorder("Direction"));
+            ButtonGroup buttonGroup = new ButtonGroup();
+            buttonGroup.add(upRadio);
+            buttonGroup.add(downRadio);
+        }
+        
+        JPanel checkBoxPanel = new JPanel(new BorderLayout());
+        inputPanel.add(checkBoxPanel, BorderLayout.WEST);
+        matchCase = new JCheckBox(new DialogAction.MatchCaseAction());
+        addEscapeToActionMap(matchCase);
+        checkBoxPanel.add(matchCase, BorderLayout.SOUTH);
+
+        JPanel eastPanel = new JPanel(new BorderLayout());
+        mainPanel.add(eastPanel, BorderLayout.EAST);
+        eastPanel.setBorder(BorderFactory.createEmptyBorder(0, 15, 5, 5));
+        JPanel buttonPanel = new JPanel(new GridLayout(0, 1, 5, 5));
+        eastPanel.add(buttonPanel, BorderLayout.NORTH);
+        findNextButton = new JButton(new EditAction.FindNextAction(jNotepad, this));
+        addEscapeToActionMap(findNextButton);
+        buttonPanel.add(findNextButton);
+        findNextButton.setEnabled(false);
+        JRootPane rootPane = getRootPane();
+        rootPane.setDefaultButton(findNextButton);
+        if (replace) {
+            replaceButton = new JButton(new DialogAction.ReplaceAction(jNotepad, this));
+            addEscapeToActionMap(replaceButton);
+            buttonPanel.add(replaceButton);
+            replaceButton.setEnabled(false);
+            replaceAllButton = new JButton(new DialogAction.ReplaceAllAction(jNotepad, this));
+            addEscapeToActionMap(replaceAllButton);
+            buttonPanel.add(replaceAllButton);
+            replaceAllButton.setEnabled(false);
+        }
+        JButton cancel = new JButton("Cancel");
+        addEscapeToActionMap(cancel);
+        buttonPanel.add(cancel);
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+                dispose();
+            }
+        });
+        
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                dispose();
+            }
+            
+        });
+
+        setResizable(false);
+        pack();
+        findField.requestFocusInWindow();
+        centerDialog();
+        findField.getDocument().addDocumentListener(this);
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+        checkIfText();
+    }
+    
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+        checkIfText();
+    }
+    
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+        checkIfText();
+    }
+    
+    private void checkIfText() {
+        if (findField.getText().isEmpty()) {
+            findNextButton.setEnabled(false);
+            if (replaceButton != null) {
+                replaceButton.setEnabled(false);
+            }
+            if (replaceAllButton != null) {
+                replaceAllButton.setEnabled(false);
+            }
+        } else {
+            findNextButton.setEnabled(true);
+            if (replaceButton != null) {
+                replaceButton.setEnabled(true);
+            }
+            if (replaceAllButton != null) {
+                replaceAllButton.setEnabled(true);
+            }
+        }
+    }
+    
+    /**
+     * @return the findTerm
+     */
+    public String getFindTerm() {
+        return findField.getText();
+    }
+    
+    /**
+     * @return the replaceTerm
+     */
+    public String getReplaceTerm() {
+        return replaceField.getText();
+    }
+    
+    /**
+     * @return the findDownDirection
+     */
+    public boolean isFindDirectionDown() {
+        if (replace) {
+            return true;
+        } else {
+            return downRadio.isSelected();
+        }
+    }
+    
+    /**
+     * @return the matchCase
+     */
+    public boolean isMatchCase() {
+        return matchCase.isSelected();
+    }
+}
